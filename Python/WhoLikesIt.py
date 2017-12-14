bdcb241daa9b594b79915f78e10769a326cd4b05
Dyeh310 <nheyd9292@gmail.com>
2017-12-13 21:30:11 -0500

    Create WhoLikesIt.py

---
+++ Python/WhoLikesIt.py
@@ -0,0 +1,20 @@
+names = ['Alex', 'Jacob', 'Mark', 'Max']
+
+def likes(names):
+    
+    textToReturn = ""
+    
+    if not names:
+        textToReturn='no one likes this'
+    elif len(names)== 1:
+        textToReturn = names[0] + ' likes this'
+    elif len(names) == 2:
+        textToReturn = names[0] + ' and ' + names[1] + ' like this'
+    elif len(names) == 3:
+        textToReturn = names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'
+    else:
+        textToReturn = names[0] + ', ' + names[1] + ' and ' + str(len(names) - 2) + ' others like this'
+    return textToReturn
+
+
+print(likes(names))
